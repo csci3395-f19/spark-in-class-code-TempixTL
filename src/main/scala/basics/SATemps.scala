9be42fb2b09dae172fcba1ac0d46e392a565ef55
TempixTL <tempixtl@icloud.com>
2019-08-27 21:24:19 -0500

    Finished Scala practice Added data file locally

--- src/main/scala/basics/SATemps.scala
+++ src/main/scala/basics/SATemps.scala
@@ -9,18 +9,37 @@ object SATemps {
   }
 
   def main(args: Array[String]): Unit = {
-    val source = scala.io.Source.fromFile("/users/mlewis/CSCI3395-F19/InClassBD/data/SanAntonioTemps.csv")
+    val source = scala.io.Source.fromFile("data/SanAntonioTemps.csv")
     val lines = source.getLines()
     val data = lines.drop(2).map(parseLine).toArray
     
     val highestHigh = data.maxBy(_.hiTemp)
-    println(s"${highestHigh.month}/${highestHigh.day}/${highestHigh.year}")
+    println(s"Date of highest temperature: ${highestHigh.month}/${highestHigh.day}/${highestHigh.year}")
 
     val mostPrecip = data.maxBy(_.precipitation)
-    println(s"${mostPrecip.month}/${mostPrecip.day}/${mostPrecip.year}")
+    println(s"Date of highest precipitation: ${mostPrecip.month}/${mostPrecip.day}/${mostPrecip.year}")
 
     val daysWithRain = data.count(_.precipitation > 1)
     val percentDaysWithRain = (daysWithRain / data.length.toDouble) * 100
-    println(f"$percentDaysWithRain%2.2f%%")
+    println(f"Percent of days with rain: $percentDaysWithRain%2.2f%%")
+
+    val rainyDays = data.filter(_.precipitation > 0)
+    val avgHiForRainy = rainyDays.foldLeft(0.0)((acc, row) => acc + (row.hiTemp / rainyDays.length))
+    println(s"Average hi temp for rainy days: $avgHiForRainy")
+
+    val months = data.groupBy(_.month)
+
+    val avgHiTempsPerMonth = months.mapValues((temps) => temps.map(_.hiTemp).sum / temps.length)
+    println("Average hi temps per month:")
+    (1 to 12).foreach((month) => println(s"Month $month: ${avgHiTempsPerMonth(month)}"))
+
+    val avgPrecipPerMonth = months.mapValues((temps) => temps.map(_.precipitation).sum / temps.length)
+    println("Average precipitation per month:")
+    (1 to 12).foreach((month) => println(s"Month $month: ${avgPrecipPerMonth(month)}"))
+
+    val precipPerMonth = months.mapValues((temps) => temps.map(_.precipitation).sorted)
+    val medianPrecipPerMonth = precipPerMonth.mapValues((precip) => precip(precip.length / 2))
+    println("Median precipitation per month:")
+    (1 to 12).foreach((month) => println(s"Month $month: ${medianPrecipPerMonth(month)}"))
   }
 }
